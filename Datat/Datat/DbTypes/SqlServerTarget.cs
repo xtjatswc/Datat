31fb79fc85cfec73f83e2f42b668752becf77e57
[qióng] <xtjatswc@users.noreply.github.com>
2018-04-04 10:29:45 +0800

    1

--- Datat/Datat/DbTypes/SqlServerTarget.cs
+++ Datat/Datat/DbTypes/SqlServerTarget.cs
@@ -8,56 +8,57 @@ using System.Threading.Tasks;
 
 namespace Datat.DbTypes
 {
-    public class SqlServerTarget : AbsDBTarget
-    {
-        public SqlServerTarget(DbParam param)
-        {
-            this.param = param;
-        }
+	public class SqlServerTarget : AbsDBTarget
+	{
+		public SqlServerTarget(DbParam param)
+		{
+			this.param = param;
+		}
 
-        public override IDbContext GetDbContext()
-        {
-            return new DbContext().ConnectionStringName(param.TargetConnName,
-        new SqlServerProvider());
-        }
+		public override IDbContext GetDbContext()
+		{
+			return new DbContext().ConnectionStringName(param.TargetConnName,
+				new SqlServerProvider());
+		}
 
-        public override void GetCreateTableSql(DataTable tbl, out List<object> lstParams, out string sql)
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.Append("select ");
+		public override void GetCreateTableSql(DataTable tbl, out List<object> lstParams, out string sql)
+		{
+			StringBuilder sb = new StringBuilder();
+			string modStr = " ALTER TABLE [" + param.TargetTblName + "] ";
+			StringBuilder sbModifyType = new StringBuilder();
 
-            lstParams = new List<object>();
-            for (int i = 0; i < tbl.Columns.Count; i++)
-            {
-                DataRow row = tbl.NewRow();
-                switch (tbl.Columns[i].DataType.ToString())
-                {
-                    case "System.Int32":
-                        row[i] = 0;
-                        break;
-                    case "System.Int64":
-                        row[i] = 0;
-                        break;
-                    case "System.Single":
-                        row[i] = 0;
-                        break;
-                    case "System.String":
-                        row[i] = null;
-                        break;
-                    case "System.DateTime":
-                        row[i] = DateTime.Now;
-                        break;
-                    default:
-                        row[i] = null;
-                        break;
-                }
+			sb.Append("select ");
 
-                sb.AppendFormat(" @{0} {1},", i, tbl.Columns[i].ColumnName);
-                lstParams.Add(row[i]);
-            }
+			lstParams = new List<object>();
+			for (int i = 0; i < tbl.Columns.Count; i++) {
+				DataRow row = tbl.NewRow();
+				switch (tbl.Columns[i].DataType.ToString()) {
+					case "System.Int32":
+						row[i] = 0;
+						break;
+					case "System.Int64":
+						row[i] = 0;
+						break;
+					case "System.Single":
+						sbModifyType.AppendLine(modStr + " ALTER COLUMN [" + tbl.Columns[i].ColumnName + "] money NULL;");
+						break;
+					case "System.String":
+						row[i] = null;
+						break;
+					case "System.DateTime":
+						row[i] = DateTime.Now;
+						break;
+					default:
+						row[i] = null;
+						break;
+				}
 
-            sql = "";
-            sql = sb.ToString().TrimEnd(',') + string.Format(" into {0};TRUNCATE TABLE {0};", param.TargetTblName);
-        }
-    }
+				sb.AppendFormat(" @{0} {1},", i, tbl.Columns[i].ColumnName);
+				lstParams.Add(row[i]);
+			}
+
+			sql = "";
+			sql = sb.ToString().TrimEnd(',') + string.Format(" into {0};TRUNCATE TABLE {0};", param.TargetTblName) + sbModifyType;
+		}
+	}
 }
